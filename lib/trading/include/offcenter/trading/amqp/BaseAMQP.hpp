dafe88ea05fa56a315c9e4d637c87fed29ed4fd2
CodeRancher <37150971+CodeRancher@users.noreply.github.com>
2021-12-22 18:24:43 -0700

    Code cleanup

--- lib/trading/include/offcenter/trading/amqp/BaseAMQP.hpp
+++ lib/trading/include/offcenter/trading/amqp/BaseAMQP.hpp
@@ -1,7 +1,7 @@
 /**
  * Copyright 2021 Scott Brauer
  *
- * Licensed under the Apache License, Version 2.0 (the );
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -19,6 +19,7 @@
  * @author Scott Brauer
  * @date   05-10-2021
  */
+
 #ifndef OFFCENTER_TRADING_AMQP_BASEPRODUCER_HPP_
 #define OFFCENTER_TRADING_AMQP_BASEPRODUCER_HPP_
 
